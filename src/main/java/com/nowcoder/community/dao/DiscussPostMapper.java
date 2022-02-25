a581078ae60d1cb1a3591d310079b40afe2e859b
ColorQian <qjl@njust.edu.cn>
2022-02-25 21:13:27 +0800

    首次提交(完成分页功能)

---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,21 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    //根据用户id查询用户发了哪些帖子(用在客户首页)
+    //offset表示每一页起始行行号，limit表示每一页最多查询多少数据
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    //@Param注解用于给参数起别名
+    //如果只有一个参数，并且在<if>里使用，则必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+}
---
+++ src/test/java/com/nowcoder/community/MapperTests.java
@@ -0,0 +1,72 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectUser() {
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        userMapper.selectByName("liubei");
+        System.out.println(user);
+
+        userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user);
+    }
+
+    @Test
+    public void testInsert() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123");
+        user.setSalt("abc");
+        user.setEmail("aksaljf@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int row = userMapper.insertUser(user);
+        System.out.println(row);
+    }
+
+    @Test
+    public void testUpdate() {
+        int status = userMapper.updateStatus(150, 1);
+
+        int pic = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
+
+        int password = userMapper.updatePassword(150, "123546");
+
+        System.out.println(status + " " + pic + " " + password);
+    }
+
+    @Test
+    public void testSelectDiscussPost() {
+        List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for (DiscussPost d : discussPosts) {
+            System.out.println(d);
+        }
+
+        int row = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(row);
+    }
+}
