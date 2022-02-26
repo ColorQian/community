a79da169c1b67e14cc45988089eab15fd36a6468
ColorQian <qjl@njust.edu.cn>
2022-02-26 11:22:34 +0800

    完成发送邮件功能

---
+++ src/test/java/com/nowcoder/community/MailSendTest.java
@@ -0,0 +1,37 @@
+package com.nowcoder.community;
+
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailSendTest {
+
+    @Autowired
+    private MailClient mailClient;  //自己封装的一个发送邮件的类，通过sendMail(String to, String subject, String content)方法发送
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void sendMail() {
+        mailClient.sendMail("1255574204@qq.com", "Test", "welcome use spring mail");
+    }
+
+    @Test
+    public void testHTMLMail() {  //发送一个HTML动态网页，给指定邮箱，使用thymeleaf模板引擎渲染
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("1255574204@qq.com", "HTML", content);
+    }
+}
