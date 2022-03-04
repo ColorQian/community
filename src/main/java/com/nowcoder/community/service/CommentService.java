6336f04930f5cc5286e00af1fb35eafa5d63c7b1
ColorQian <qjl@njust.edu.cn>
2022-03-04 15:54:08 +0800

    完成显示评论、添加评论功能

---
+++ src/main/java/com/nowcoder/community/service/CommentService.java
@@ -0,0 +1,57 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.CommentMapper;
+import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.SensitiveFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.util.HtmlUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CommentService implements CommunityConstant {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Resource
+    private DiscussPostService discussPostService;
+
+    public List<Comment> findCommentsByEntity(int entityType, int entityId, int offset, int limit) {
+        return commentMapper.selectCommentByEntity(entityType, entityId, offset, limit);
+    }
+
+    public int findCommentCount(int entityType, int entityId) {
+        return commentMapper.selectCountByEntity(entityType, entityId);
+    }
+
+    //需要进行事务管理
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public int addComment(Comment comment) {
+
+        if (comment == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+
+        //需要对传入的comment实体进行过滤
+        comment.setContent(HtmlUtils.htmlEscape(comment.getContent()));
+        comment.setContent(sensitiveFilter.filter(comment.getContent()));
+        int rows = commentMapper.insertComment(comment);
+
+        //更新帖子评论数量
+        if (comment.getEntityType() == ENTITY_TYPE_POST) {
+            int count = commentMapper.selectCountByEntity(comment.getEntityType(), comment.getEntityId());
+            discussPostService.updateCommentCount(comment.getEntityId(), count);
+        }
+        return rows;
+    }
+}
