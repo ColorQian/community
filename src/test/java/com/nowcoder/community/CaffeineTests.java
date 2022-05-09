6cc1b951769e6c7f3fae091b3ecd0bf7802a9738
ColorQian <qjl@njust.edu.cn>
2022-05-09 21:08:02 +0800

    加本地缓存Caffeine

---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+
+    @Test
+    public void testCache() {
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 0));
+    }
+}
